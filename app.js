1025b02800df6ba6d34889255ce3512c80eff01a
ShashiSahani <shashisahani496@gmail.com>
2025-02-19 13:44:52 +0530

    update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello, welcome to my API!');
+});
 app.use('/tasks', taskRoutes);
 
 connectDB();
