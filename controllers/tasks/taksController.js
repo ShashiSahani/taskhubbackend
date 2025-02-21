1ae1cd22ec7463e5669175016e5c5b38678ec1f9
ShashiSahani <shashisahani496@gmail.com>
2025-02-21 09:01:10 +0530

    update gettaskbyid

--- controllers/tasks/taksController.js
+++ controllers/tasks/taksController.js
@@ -43,3 +43,15 @@ exports.deleteTask=async(req,res)=>{
         res.status(500).json({message:error.message})
     }
 }
+
+exports.getTasksById=async(req,res)=>{
+  try {
+    const task=await Task.findById(req.params.id)
+    if(!task){
+      return res.status(404).json({message:"Task not found"});
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({message:"Internal Server Error"})
+  }
+}
--- routes/tasks/taskRoutes.js
+++ routes/tasks/taskRoutes.js
@@ -1,12 +1,13 @@
 const express=require('express');
 
 
-const {getTasks,createTask,updateTask,deleteTask}=require('../../controllers/tasks/taksController');
+const {getTasks,createTask,updateTask,deleteTask,getTasksById}=require('../../controllers/tasks/taksController');
 
 const router=express.Router();
 
 
 router.get("/",getTasks);
+router.get("/:id",getTasksById);
 router.post("/",createTask);
 router.put("/:id",updateTask);
 router.delete("/:id",deleteTask);
